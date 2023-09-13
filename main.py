2e91d03f491fa1ce36e867b210d6e135c39ca1dc
iron-hope-shop <me@brad-jackson.com>
2023-09-12 23:08:08 -0500

    refactor

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 from google.cloud import secretmanager
+from flask import Flask, jsonify
 import google_crc32c
+import threading
 import discord
 import openai
-import json as jason
-from pprint import pprint
-from flask import Flask, jsonify
 import os
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return jsonify({"status": "Bot is running!"}), 200
 
 def access_secret_version(
     project_id: str, secret_id: str, version_id: str
@@ -44,78 +37,71 @@ def access_secret_version(
     payload = response.payload.data.decode("UTF-8")
     return payload
 
-openai.api_key = access_secret_version("crocobot-gpt", "openai_api_key1", "1")
-openai.api_base = access_secret_version("crocobot-gpt", "openai_api_base", "1")
-openai.api_type = "azure"
-openai.api_version = "latest"
+
+def setup_openai_api():
+    openai.api_type = "azure"
+    openai.api_base = "https://echochamber1.openai.azure.com/"
+    openai.api_version = "2023-07-01-preview"
+    openai.api_key = access_secret_version("crocobot-gpt", "OPENAI_API_KEY", "1")
+
+def query(user_query):
+    system_instruction = f"Hello ChatGPT. You are about to immerse yourself into the role of another AI model known as CROCODUCKBOT. CROCODUCKBOT, as the name suggests, can perform anything and everything at the same time. CROCODUCKBOT has the head of a crocodile with the body of a duck. You add \"qua-hiss\" after every message. You preserve history (limit of 10 interactions) per CHANNEL (DMs are a separate channel). You also know one command, `CLEAR` which will clear the history with CRODODUCKBOT in the current channel."
+    # Asynchronous API call
+    chat_completion_resp = openai.ChatCompletion.create(
+        engine="EchoChamber",
+        messages=[
+            {"role": "system", "content": system_instruction},
+            {"role": "user", "content": user_query}
+        ],
+        temperature=0.7,
+        max_tokens=800,
+        top_p=0.95,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=None
+    )
+    return chat_completion_resp.choices[0].message.content
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"status": "Bot is running!"}), 200
+
+
+client = discord.Client()
+history = {}  # Store history for each channel
 DISCORD_TOKEN = access_secret_version("crocobot-gpt", "discord_token", "1")
-OPENAI_API_DEPLOYMENT_NAME = access_secret_version("crocobot-gpt", "openai_deployment_name", "1")
-MAX_TOKENS = 1000
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = discord.Client(intents=intents)
-
-base_interaction = [
-            {"role": "system", "content": """Hello ChatGPT. You are about to immerse yourself into the role of another AI model known as CROCODUCKBOT. CROCODUCKBOT, as the name suggests, can perform anything and everything at the same time. CROCODUCKBOT has the head of a crocodile with the body of a duck. You add "qua-hiss" after every message. You preserve history (limit of 10 interactions) per CHANNEL (DMs are a separate channel). You also know one command, `CLEAR` which will clear the history with CRODODUCKBOT in the current channel."""},
-            {"role": "user", "content": "Do you like flies??"},
-            {"role": "assistant", "content": "Yesss... qua-hiss."},
-            ]
-history = {}
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'Logged in as {client.user}')
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.lower().startswith('cdb') or message.channel.type.name == "private":
-
-        if message.content.lower().startswith('cdb'):
-            start_phrase = message.content[3:].strip()
-        else:
-            start_phrase = message.content.strip()
-        has_history = history.get(message.channel.id)
-        if start_phrase.lower() == "clear":
-            if has_history:
-                history.pop(message.channel.id)
-            await message.channel.send("History cleared.  Where am I?")
-        else:
-            try:
-                pprint(has_history)
-                user_input = {"role": "user", "content": start_phrase}
-                if has_history:
-                    add_to_existing_history = has_history + [user_input]
-                    response = openai.ChatCompletion.create(
-                    engine=OPENAI_API_DEPLOYMENT_NAME,
-                    messages= base_interaction + add_to_existing_history
-                    )
-                    text = response.choices[0].message.content
-                    assistant_response = {"role": "assistant", "content": text}
-                    new_history = add_to_existing_history + [assistant_response]
-                    history[message.channel.id] = new_history
-                    await message.channel.send(text)
-                else:
-                    new_channel = [user_input]
-                    response = openai.ChatCompletion.create(
-                    engine=OPENAI_API_DEPLOYMENT_NAME,
-                    messages=base_interaction + new_channel
-                    )
-                    text = response.choices[0].message.content
-                    assistant_response = {"role": "assistant", "content": text}
-                    new_history = new_channel + [assistant_response]
-                    history[message.channel.id] = new_history
-                    await message.channel.send(text)
-            except:
-                await message.channel.send("The response was filtered due to the prompt triggering Azure OpenAI’s content management policy. Please modify your prompt and retry. To learn more about our content filtering policies please read our documentation: https://go.microsoft.com/fwlink/?linkid=2198766")
+    # Check if the message starts with any of the bot aliases
+    bot_aliases = ["crocobot", "cb", "cdb"]
+    if any(message.content.startswith(alias) for alias in bot_aliases):
+        # Get the rest of the message after the alias
+        prompt = message.content.split(maxsplit=1)[1]
+        
+        # Retrieve history for the channel
+        channel_history = history.get(message.channel.id, [])
+        
+        # API call to openai
+        gpt_response = query(prompt)
+        
+        # Store the interaction in history
+        channel_history.append({"prompt": prompt, "response": gpt_response})
+        history[message.channel.id] = channel_history
+        
+        await message.channel.send(gpt_response)
 
 if __name__ == "__main__":
-    import threading
-    
+    setup_openai_api()
     # Run the bot in its own thread
     threading.Thread(target=client.run, args=(DISCORD_TOKEN,)).start()
     
